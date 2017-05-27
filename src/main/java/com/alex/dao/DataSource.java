e499ead5908d02e711b9bf39c69451cb97fb5d01
Oleksiy <o_orlovskiy@ukr.net>
2017-05-27 15:44:34 +0300

    update

--- src/main/java/com/alex/dao/DataSource.java
+++ src/main/java/com/alex/dao/DataSource.java
@@ -3,9 +3,13 @@ package com.alex.dao;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 import java.beans.PropertyVetoException;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
+import java.util.ResourceBundle;
 
 public class DataSource {
 
@@ -13,15 +17,17 @@ public class DataSource {
     private ComboPooledDataSource cpds;
 
     private DataSource() throws IOException, SQLException, PropertyVetoException {
+        //get data from resourses/database.properties
+        ResourceBundle resource = ResourceBundle.getBundle("database");
         cpds = new ComboPooledDataSource();
         cpds.setDriverClass("org.postgresql.Driver");
-        cpds.setJdbcUrl("jdbc:postgresql://127.0.0.1:5432/rest");
-        cpds.setUser("postgres");
-        cpds.setPassword("123456");
+        cpds.setJdbcUrl(resource.getString("db.url"));
+        cpds.setUser(resource.getString("db.user"));
+        cpds.setPassword(resource.getString("db.password"));
 
         cpds.setMinPoolSize(5);
         cpds.setAcquireIncrement(5);
-        cpds.setMaxPoolSize(20);
+        cpds.setMaxPoolSize(10);
         cpds.setMaxStatements(180);
 
     }
